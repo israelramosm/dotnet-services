d6abff6dc074c8cfa3ea0c56704737ad88de33ce
Israel Ramos <arnulfo_israel@hotmail.com>
2019-12-08 14:47:22 -0600

    Todo item added, db context added

---
+++ DotNetRestServices/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace DotNetRestServices
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
