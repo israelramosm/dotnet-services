55d93ca14a3f7d4ef8a7cc748f4cf03dc2d921ee
Israel Ramos <arnulfo_israel@hotmail.com>
2019-12-08 20:48:35 -0600

    TodoContext fixed

--- DotNetRestServices/Models/TodoContext.cs
+++ DotNetRestServices/Models/TodoContext.cs
@@ -4,10 +4,9 @@ namespace DotNetRestServices.Models
 {
     public class TodoContext : DbContext
     {
-        public TodoContext(DbContextOptions<TodoContext> options)
+        public TodoContext(DbContextOptions<TodoContext> options) 
         : base(options)
-        {
-            public DbSet<TodoItem> TodoItems { get; set; }
-        }
+        { }
+        public DbSet<TodoItem> TodoItems { get; set; }
     }
 }
